ae3d504a9ad6f426903b438a47bfb80a725e14a3
jebaile7964 <bailey.jonathan@hotmail.com>
2015-04-02 13:44:24 -0500

    Added new CMDlets.

---
+++ Five-Liners/Rename-SSSPC.ps1
@@ -0,0 +1,32 @@
+﻿Function Rename-SSSPC{
+    <#
+    .AUTHOR
+        Jonathan Baily and Tyler Brown
+    .SYNOPSIS 
+       Renames a computer. 
+    .DESCRIPTION
+       Renames the computer by grabing the serial number off the motherboard and replacing the current computer name.
+    .EXAMPLE
+       Rename-SSSPC
+    .INPUTS
+       No inputs required.  This CMDlet pulls information from the motherboard of the PC.
+    .OUTPUTS
+       No outputs.  The data is automatically piped to the Rename-Computer CMDlet.
+    .NOTES
+       This CMDlet requires no parameters.  This CMDlet only runs on PowerShell 4.0.
+    .COMPONENT
+       This CMDlet belongs to the SSSCMDlets Module.
+    #>
+    CMDletbinding[( DefaultParameterSetName ='Static', 
+                    SupportsShouldProcess=$true, 
+                    PositionalBinding=$false
+                 )]
+    BEGIN {}
+    PROCESS {
+        $serial = gcim win32_bios | select -Property serialnumber | ft -HideTableHeaders
+        $serial = $serial | out-string
+        $serial = $serial.trim( )
+        Rename-Computer -ComputerName (gcim win32_operatingsystem).CSName -NewName $serial
+    }
+    END{}
+}
---
+++ Shortcut Creation/Create-ShortcutIcons.ps1
@@ -0,0 +1,105 @@
+﻿Function Create-ShortcutIcons{
+<#
+.NAME
+    Create-Shortcuts
+.AUTHOR
+    Jonathan Bailey
+.SYNOPSIS
+    Creates shortcuts corresponding to the proper workstation IDs.
+.DESCRIPTION
+    Can either pull shortcut IDs from a CSV, or from a set of IDs provided as a parameter.
+.EXAMPLE
+    Create-Shortcuts -WSID AA,A1,A2 -RPGDirectory "I:\RPG" -Version "SSS" -Destination "I:\icons"
+.EXAMPLE
+    Create-Shortcuts -CSV "D:\icons.csv" -RPGDirectory "D:\RPG" -Version "Propane" -Destination "D:\icons"
+.INPUTS
+    Can either take indivdual or multiple WSIDs, or a CSV file with WSIDs.  If using a CSV, please label
+    the column WSID.
+.OUTPUTS
+    Creates a shortcut link in the provided destination folder.
+.COMPONENT
+    This CMDlet is part of SSSCMDlets Module.
+#>
+    [CmdletBinding(DefaultParameterSetName ='WSID', 
+                   SupportsShouldProcess=$true, 
+                   PositionalBinding=$false
+                  )]
+    Param
+    (
+        # Used in declaring WSID manually.
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=0,
+                   ParameterSetName='WSID')]
+        $WSID,
+
+        [Parameter( Mandatory = $true,
+                    ValueFromPipeline = $true,
+                    ValueFromPipelineByPropertyName = $true,
+                    Position = 1
+                  )]
+        $RPGDirectory,
+
+        # Choose SSS or Propane
+        [Parameter( Mandatory = $true,
+                    ValueFromPipeline = $false,
+                    ValueFromRemainingArguments = $false,
+                    Position = 2
+                  )]
+        [ValidateSet("SSS","Propane")]
+        $Version,
+
+        [Parameter( Mandatory = $true,
+                    ValueFromPipeline = $true,
+                    ValueFromRemainingArguments = $false,
+                    Position = 3
+                  )]
+        [String]
+        $Destination,
+
+        # Param3 help description
+        [Parameter( ParameterSetName='CSV',
+                    Mandatory = $true,
+                    ValueFromPipeline = $true,
+                    ValueFromRemainingArguments = $false,
+                    Position = 4
+                  )]
+        [String]
+        $CSV
+    )
+
+    Begin{
+        $WSID = @()
+        Set-Location $Destination
+    }
+    Process{
+        If ($CSV -ne $null){
+            $CSVar = Import-Csv $CSV
+            $CSVar | foreach {
+                $WSID += $_.WSID
+            }
+        }
+        If($Version -eq "SSS"){
+            $RPGProgram = Join-Path $RPGDirectory -ChildPath "#library\b36run.exe"
+            $IconName = "SSS"
+        }
+        If ($Version -eq "Propane"){
+            $RPGProgram = Join-Path $RPGDirectory -ChildPath "vblib\propane.exe"
+            $IconName = "Propane"
+        }
+
+        # Create a Shortcut with Windows PowerShell
+        $WSID | foreach {
+            $TargetFile = "$RPGProgram $WSID"
+            $ShortcutFile = "$Destination\$IconName $WSID.lnk"
+            $WScriptShell = New-Object -ComObject WScript.Shell
+            $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+            $Shortcut.TargetPath = $TargetFile
+            $Shortcut.Save()
+        }
+    }
+    END{}
+}
+
