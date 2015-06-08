d408ae00d2e5c0a0e7c5e90e98088d52815c5e49
jebaile7964 <bailey.jonathan@hotmail.com>
2015-06-08 15:59:15 -0500

    Create WikiScrape.py

---
+++ Python/WikiScrape.py
@@ -0,0 +1,15 @@
+## This script was tested using Python 2.7.9
+## The MWClient library was used to access the api.  It can be found at:
+## https://github.com/mwclient/mwclient
+
+import mwclient
+site = mwclient.Site(('https', 'en.wikipedia.org'))
+site.login('$user', '$pass') # credentials are sanitized from the script.
+listpage = site.Pages['User:jebaile7964/World_of_Warcraft']
+text = listpage.text()
+for page in site.Categories['World_of_Warcraft']:
+    text += "* [[:" + page.name + "]]\n"
+listpage.save(text, summary='Creating list from [[Category:World_of_Warcraft]]')
+
+## results are found at:
+## https://en.wikipedia.org/wiki/User:Jebaile7964/World_of_Warcraft
