b60805cfdf7806b63a6ef1485503f08629333612
jebaile7964 <bailey.jonathan@hotmail.com>
2015-08-21 09:03:19 -0500

    Update Get-UsersNotLoggedIn.ps1

--- AD/Get-UsersNotLoggedIn.ps1
+++ AD/Get-UsersNotLoggedIn.ps1
@@ -1,9 +1,9 @@
-$session = New-PSSession -ComputerName RDPVM21
+$session = New-PSSession -ComputerName $ComputerName
 
 Invoke-Command -ScriptBlock {Import-Module ActiveDirectory} -Session $session
 Import-PSSession -Module activedirectory -Session $session
 
-$ou = Get-ADOrganizationalUnit -Filter * -Properties * | where-object name -match 'Midwestern'
+$ou = Get-ADOrganizationalUnit -Filter * -Properties * | where-object name -match "$OUName"
 
 $users = Get-ADUser -Filter * -Properties * | Where-Object distinguishedname -Match $ou.distinguishedname
 $users | Select-Object -name -Property lastlogondate | Where-Object lastlogon -eq 0
